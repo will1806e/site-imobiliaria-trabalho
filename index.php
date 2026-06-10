6f06b3cebd77713d4058c03204f1eec5304baf8b
Willian <alewillreis@gmail.com>
2026-06-09 23:06:10 -0300

    alguns icones e tela inicial comecei a fazer

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 </head>
 <body>
     <header>
-        
+        <img src="../site_imobiliaria/icones/menu.png" alt="Menu" width="50">
+        <img src="../site_imobiliaria/icones/logo.png" alt="Logo" width="150">
+        <img src="../site_imobiliaria/icones/account_circle.png" alt="Usuario">
+        <hr>
     </header>
 </body>
 </html>
